36f7971697d6e46b164643f2bb57fc89114b0192
Alex Forey <me@alexforey.com>
2014-01-03 13:22:18 +0000

    Initial Commit

---
+++ design/f.php
@@ -0,0 +1,28 @@
+		<section  class="fullwidth dark-grey flat footer inset">
+			<div class="container">
+				<footer>
+					<div class="info">
+						<p><span>Designed by Alex Forey with Coda 2</span></p>
+						<p>&copy; Alex Forey 2011 - <?php echo date('Y'); ?></p>
+					</div>
+				</footer>
+			</div>
+		</section>
+
+		<script type="text/javascript">
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-28573924-1']);
+			_gaq.push(['_setDomainName', 'alexforey.com']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+
+		</script>
+		
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,139 @@
+<?php include 'design/h.php'; ?>
+
+<section class="fullwidth dark" id="front-feature">
+	<div class="container">
+	
+		<div class="heading">
+			<h1>Hi, this is Alex Forey
+				<small>Getting Excited and Making Things since 1997</small>
+			</h1>
+		</div>
+		
+		<div class="featured-project video fitvid">
+			<iframe src="http://player.vimeo.com/video/83308624?title=0&amp;byline=0&amp;portrait=0&amp;color=da594c" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+		</div>
+		
+		<div class="see-more">
+			<a class="arrow-btn" href="/portfolio">
+				<div class="arrow left"><span></span></div>
+				<span class="text">See more of my work</span>
+				<div class="arrow right"><span></span></div>
+			</a>
+		</div>
+		
+	</div>
+</section>
+
+<section class="fullwidth flushbottom">
+	<div class="container">
+	
+		<div class="heading">
+			<h1>I make things</h1>
+			<p>I make short films, take photos, make timelapses, design and develop websites, make Little Printer Publications and film live events.</p>
+		</div>
+	
+		<div id="somelinks">
+			
+			<a class="work-section" href="https://markpond.com">
+				<div class="icon" id="markpond"></div>
+				<h2>Markpond</h2>
+				<p>A bookmarking website with offline archiving, a Little Printer publication and much more; written in Ruby on Rails.</p>
+				
+				<div class="arrow-btn white">
+					<div class="arrow left"><span></span></div>
+					<span class="text">Take a look</span>
+					<div class="arrow right"><span></span></div>
+				</div>
+			</a>
+			
+			<a class="work-section" href="http://films.alexforey.com">
+				<div class="icon" id="cinematicmob"></div>
+				<h2>Cinematic Mob</h2>
+				<p>I direct and shoot short films with little or no budget for fun. My most recent film, <em>Perception</em>, was released recently.</p>
+				
+				<div class="arrow-btn white">
+					<div class="arrow left"><span></span></div>
+					<span class="text">Take a look</span>
+					<div class="arrow right"><span></span></div>
+				</div>
+			</a>
+			
+			<a class="work-section" href="http://shwiing.co">
+				<div class="icon" id="shwiing"></div>
+				<h2>Shwiing</h2>
+				<p>A simple way for your Arduino to store data in the cloud, written in PHP and interfacing with Twitter.</p>
+				
+				<div class="arrow-btn white">
+					<div class="arrow left"><span></span></div>
+					<span class="text">Take a look</span>
+					<div class="arrow right"><span></span></div>
+				</div>
+			</a>
+			
+			<a class="work-section" href="http://infinitevideo.co.uk">
+				<div class="icon" id="infinitevideo"></div>
+				<h2>Infinite Video</h2>
+				<p>My production company, filming live events and producing DVDs for school plays.</p>
+				
+				<div class="arrow-btn white">
+					<div class="arrow left"><span></span></div>
+					<span class="text">Take a look</span>
+					<div class="arrow right"><span></span></div>
+				</div>
+			</a>
+						
+			<div class="clear"></div>
+		</div>
+	</div>
+</section>
+
+<section class="fullwidth red info">
+	<div class="container">
+		<p>I'm interested in making websites. In fact the one you're looking at now was made by me, with only Coda 2 for the code and Photoshop for the images and sprites.</p>
+		<p>I like the idea of things integrating with each other, and this is one of my main uses of the internet. I use <a href="http://ifttt.com">If This Then That</a> a lot to help me achieve this. Here's how just a little bit of my internet works: I take photos using Instagram or Flickr, depending on their privacy policies and my mood at any given time. Those are then sent to Flickr automatically, and every photo on my Flickr ends up on my Tumblr-run photos.alexforey.com. But that's only the tip of the iceberg. My GitHub account <a href="https:/twitter.com/alex_commits">tweets</a>, and my Pocket articles are auto-saved into my <a href="https://markpond.com/u/alex">Markpond account</a> when I've finished reading them.</p> 
+		<p>If you're interested in how this site works, then keep reading. All the files are hosted on my own (shared) server, but the subdomain photos.alexforey.com is hosted with Tumblr, with a custom designed theme to work with the CSS files on my server. My CSS is actually SASS, which allows me to use variables for colours and fonts in my stylesheet, and nesting that means I can write more precise styles more quickly, to keep everything organised.</p>
+	</div>
+</section>
+
+<section class="fullwidth dark">
+	<div class="container">
+		<div class="heading">
+			<h1>Say Hello</h1>
+			<p>Want to say hi? Pick a method:</p>
+		</div>
+		
+		<div class="social">
+			<ul>
+				<span>
+				<li><a id="twitter" href="https://twitter.com/alexforey" target="_blank" title="Twitter"></a></li>
+				<li><a id="flickr" href="http://flickr.com/photos/alexforey" target="_blank" title="Flickr"></a></li>
+				<li><a id="github" href="https://github.com/alfo" target="_blank" title="GitHub"></a></li>
+				</span>
+				<li><a id="markpond" href="https://markpond.com/u/alex" target="_blank" title="Markpond Bookmarks"></a></li>
+				<li><a id="vimeo" href="https://vimeo.com/alexforey" target="_blank" title="Vimeo"></a></li>
+				<li id="last">
+					<script type="text/javascript">
+					//<![CDATA[
+					<!--
+					var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+					"=92){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+					".substr(0,ol);}f(\")47,\\\"qysnd{%l~771\\\\bc100\\\\OY\\\\\\\\TLWN620\\\\PF" +
+					"VE120\\\\E@Wr\\\\BNB^010\\\\CCVQ300\\\\WNY?ysusn9lv~a4aws}jch~+')dnghA!^<e2" +
+					"20\\\\t\\\\520\\\\710\\\\Z[$430\\\\030\\\\420\\\\030\\\\120\\\\-S,R230\\\\0" +
+					"10\\\\310\\\\130\\\\310\\\\530\\\\HE:n\\\\010\\\\710\\\\700\\\\)]+wvxse|1`c" +
+					"h$pmuic|fn`@120\\\\120\\\\020\\\\630\\\\220\\\\C720\\\\300\\\\230\\\\430\\\\"+
+					"520\\\\630\\\\P-Mt\\\\310\\\\730\\\\400\\\\KH5400\\\\610\\\\700\\\\010\\\\1" +
+					"00\\\\A>\\\\\\\\400\\\\6~<`yr742\\\"\\\\<&$|%>*#8/$.\\\"(f};o nruter};))++y" +
+					"(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y;i=+y)47==i(fi{)++i;l<i;0=i(" +
+					"rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                          ;
+					while(x=eval(x));
+					//-->
+					//]]>
+					</script>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+	
+<?php include 'design/f.php'; ?>
