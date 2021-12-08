58fc3011a9fa529e3d327b3d23e1f18339270663
Alex Forey <me@alexforey.com>
2021-12-08 17:55:36 +0000

    Add all the things in one go lmao

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
 
       <img class="headshot" src="/imgs/headshot.jpg">
 
-      <p>I'm an <?php echo $age; ?>-year-old lighting designer, associate, programmer, and technician. I have been working in professional theatre since 2016 across all scales of theatre, from 50-seat fringe venues to the West End. I am currently a third year student at the Royal Academy of Dramatic Art, where I started in September 2019. My work while undertaking training can <a href="https://www.rada.ac.uk/profiles/alex-forey/" target="_blank">be seen here</a>.</p>
+      <p>I'm a <?php echo $age; ?>-year-old lighting designer, associate, programmer, and technician. I have been working in professional theatre since 2016 across all scales of theatre, from fringe to the West End. I am currently a third year student at the Royal Academy of Dramatic Art, where I started in September 2019. My work while undertaking training can <a href="https://www.rada.ac.uk/profiles/alex-forey/" target="_blank">be seen here</a>.</p>
 
       <p>
         My work as lighting designer includes: <i>Polyeucte</i> (Theatre Royal Stratford East), <i>Libuše</i>, <i>Rhapsody 2019</i>, <i>From Here To Eternity</i> (Bloomsbury Theatre), <i>Babel</i> (Lyric Hammersmith), <i>Into The Woods</i>, <i>Exhibition</i> (Shaw Theatre), <i>A Sexual Odyssey</i>, <i>Written on the Body</i> (Cockpit Theatre), <i>Songs for a New World</i> (Drayton Arms Theatre), <i>The Piper of Hamelin</i> (Rose Theatre Kingston), <i>Manifestos</i> (Bloomsbury Festival, RADA Studios), <i>Graduate Showcase 2019</i> (Italia Conti Academy), <i>Henry V</i>, <i>Hiroshima: Crucible of Light</i> (Samuel Pepys Theatre), <i>Grey Matter</i>, <i>Twelfth Night</i> (Edinburgh Fringe).
@@ -34,11 +34,13 @@
 
       <p>As assistant lighting designer: <i>Growl</i> (LD Zoe Spurr, Tour: Theatre Royal Bury St Edmunds, Curve Leicester, Rose Theatre Kingston), <i>Parade</i>, <i>Legend Trippers</i>, <i>Anything Goes</i> (LD Aaron Dootson, The Other Palace).</p>
 
-      <p>As programmer: <i>Nature of Why</i> (LD Seb Blaber, Queen Elizabeth Hall, Souhtbank Centre), <i>Quintessential</i> (Peacock Theatre, Sadler’s Wells).</p>
+      <p>As lighting programmer: <i>The Beauty Queen of Leenane</i> (LD Kevin Treacy, Lyric Hammersmith), <i>Nature of Why</i> (LD Seb Blaber, Queen Elizabeth Hall, Souhtbank Centre), <i>Quintessential</i> (Peacock Theatre, Sadler’s Wells).</p>
 
-      <p>I studied Maths, Physics, and English Literature for A Level, and attended University College London for Computer Science before switching to theatre full-time. Since then, I have assisted Zoe Spurr, Andrew Ellis and others on their designs for plays, musicals, and dance, both in London and on tour. I have worked as a casual at Wyndham’s Theatre (board operator cover: <i>Red</i>, <i>The Height of the Storm</i>, <i>The Price</i>, <i>The Starry Messenger</i>; followspot cover: Bill Bailey's <i>Larks in Transit</i>), the Donmar Warehouse, Lyric Hammersmith (Chief of Projection: <i>City of Glass</i>, electrician on 10+ shows), and Shakespeare’s Globe (where I operated the last ever performance with production lighting at the end of Emma Rice’s tenure).</p>
-
+      <p>I have also worked as a lighting technician in a variety of roles — freelance electrician for Ammonite <i>Cabaret at the Kit Kat Club</i> (Playhouse Theatre), <i>Back To The Future The Musical</i> (Adelphi Theatre), <i>The Mirror and the Light</i> (Gielgud Theatre), Lyric Hammersmith (Chief of Video <i>City of Glass</i>, duty LX), Wyndham's Theatre (board operator cover: <i>RED</i>, <i>The Man in the White Suit</i>, <i>The Height of the Storm</i>, <i>The Price</i>, <i>The Starry Messenger</i>; followspot cover: Bill Bailey's <i>Larks in Transit</i>), the Donmar Warehouse, and Shakespeare’s Globe (where I operated the last ever performance with production lighting at the end of Emma Rice’s tenure).</p>
+      
       <p>I own my own <a href="https://newfangled.me/eos-nomad-setup/">EOS Nomad 6K</a> system, and have worked as an assistant EOS trainer for ETC at their London office, alongside Declan Randall. I hold Level 3 certification in EOS, as well their two Expert level certifications, and am part of the closed EOS beta testing group, catching bugs before they reach the wider userbase.</p>
+
+      <p>I studied Maths, Physics, and English Literature for A Level, and attended University College London for Computer Science before switching to theatre full-time.</p>
 
       <p>In my early years I made short films and worked as an event and corporate videographer to earn money for equipment, before a shadowing placement at the National Theatre convinced me that theatre was the path for me.</p>
 
