5dcee3b1cbdad6effd76d1a58f6d0fe871aad0b7
Alex Forey <me@alexforey.com>
2023-07-25 16:25:15 +0100

    press page

--- press.php
+++ press.php
@@ -25,7 +25,11 @@
           if ($project['press']) {
           ?>
 
-          <h1><a href="/lighting/<?php echo $project['id']?>"><?php echo $project['title']; ?></a></h1>
+          <h1>
+            <a href="/lighting/<?php echo $project['id']?>">
+              <?php echo $project['title']; ?> (<?php echo $project['date'].split(" ")[1]; ?>)
+            </a>
+          </h1>
           <div class="press">
           <?php foreach ($project['press'] as $quote => $pub) { ?>
             <blockquote><?php echo $quote; ?> <small>— <?php echo $pub; ?></small></blockquote>
