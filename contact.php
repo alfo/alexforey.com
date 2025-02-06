7de3a8bb423b5fbaf3ffbea1ee26a0af490826d1
Alex Forey <me@alexforey.com>
2025-02-06 17:46:00 +0000

    version 1 of contact page

---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php
+  $title = 'Contact';
+  include 'includes/header.php';
+?>
+
+  <div class="container">
+
+    <header class="top-of-page">
+      <h1><?php echo $title; ?></h1>
+    </header>
+
+  </div>
+
+  <div class="about-me">
+
+    <div class="container">
+
+      <img class="headshot" src="/imgs/headshot.jpg">
+
+      <p>A long paragraph goes here</p>
+
+      <div class="clear"></div>
+    </div>
+
+  </div>
+
+  <div class="elsewhere">
+
+    <h1>Contact</h1>
+
+    <div class="container">
+      <ul>
+        <span>
+        <li><a id="mail" href="mailto:hello@.alexforey.com" title="Email"></a></li>
+        <li><a id="twitter" href="https://twitter.com/alexforey" target="_blank" title="Twitter"></a></li>
+        </span>
+        <li><a id="instagram" href="https://instagram.com/alexforeylighting/" target="_blank" title="Instagram"></a></li>
+        <li><a id="vimeo" href="https://vimeo.com/alexforey" target="_blank" title="Vimeo"></a></li>
+      </ul>
+    </div>
+
+  </div>
+
+</section>
+
+<?php include 'includes/footer.php'; ?>
