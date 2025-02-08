2fa05f03ce6e882b4e00a8c75df9a33fe26dd542
Alex Forey <me@alexforey.com>
2025-02-08 18:05:30 +0000

    Auto sliders

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 
 	$('.slider').unslider({
-		infinite: true
+		infinite: true,
+		autoplay: true,
+		speed: 1000,
+		delay: 4000
 	});
 
 	$(".fitvid").fitVids();
 
 	$('.slider-with-caption').parent().addClass('contains-slider-with-caption');
+
+	$(".slider").bind('swipeleft',function(event, ui){
+		console.log("swipe");
+	})
 
 });
